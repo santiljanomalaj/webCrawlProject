712fcdad0c9b821edfc9a6418fe7b494f69391d8
lijinho <58988274+lijinho@users.noreply.github.com>
2020-04-16 14:45:37 +0800

    text phone number fix

--- scrape.php
+++ scrape.php
@@ -46,35 +46,41 @@
       $allPhoneData="";
       $location=$_POST['location'];
       $k=1;
-      for($i=$_POST['pageNumber'];$i<$_POST['pageNumber']+1;$i++)
+      for($i=$_POST['pageNumber'];$i<$_POST['pageNumber']+5;$i++)
       {
         $adsData=file_get_contents("https://www.mudah.my/".$location."/for-sale?o=".$i."&q=&so=1&th=1");
             $selectPhoneUrl=select_elements('.thumbnail_images', $adsData);
-            for($j=0;$j<20;$j++){
+            for($j=0;$j<40;$j++){
               if(isset($selectPhoneUrl[$j]['children'][2]['attributes']['href']))
               {
                   $phoneInfo=file_get_contents($selectPhoneUrl[$j]['children'][2]['attributes']['href']);
-                  $selectPhoneName=select_elements('.complex_header', $phoneInfo);
+                  $selectPhoneName=select_elements('.top_seller_name', $phoneInfo);
                   $selectContactNumber=select_elements('.moreless', $phoneInfo);
-                  $selectNumber = explode('01',$selectContactNumber[0]['text']);
-                  $phoneName=$selectPhoneName[0]['children'][0]['text'];
-                  if(isset($selectNumber[1])){
-                    // $prefixNumber = explode('.', $selectNumber[1]);
-                    // $exactPrefix = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$prefixNumber[0]);
-                    $allPhoneData.="<tr><td>".$k++."</td><td>".$phoneName."</td><td><b>01".$selectNumber[1]."</b></td></tr>";
+                  $selectNumber = explode(' 01',$selectContactNumber[0]['text']);
+                  $selectNumber2=explode('01',$selectContactNumber[0]['text']);
+                  $selectNumber = isset($selectNumber[1]) ? $selectNumber : $selectNumber2;
+                  if(isset($selectNumber[1]) && isset($selectPhoneName[0]['children'][0]['text'])){
+                    $phoneName=$selectPhoneName[0]['children'][0]['text'];
+                    $prefixNumber = preg_replace('/[^A-Za-z0-9]/', '', $selectNumber[1]);
+                    $exactPrefix = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$prefixNumber);
+                    if(strlen($exactPrefix[0])==8)
+                      $allPhoneData.="<tr><td>".$k++."</td><td>".$phoneName."</td><td><b>01".$exactPrefix[0]."</b></td></tr>";
                   }
               }
               else if(!isset($selectPhoneUrl[$j]['children'][2]['attributes']['href']))
               {
                   $phoneInfo=file_get_contents($selectPhoneUrl[$j]['children'][1]['attributes']['href']);
-                  $selectPhoneName=select_elements('.complex_header', $phoneInfo);
+                  $selectPhoneName=select_elements('.top_seller_name', $phoneInfo);
                   $selectContactNumber=select_elements('.moreless', $phoneInfo);
-                  $selectNumber = explode('01',$selectContactNumber[0]['text']);
-                  $phoneName=$selectPhoneName[0]['children'][0]['text'];
-                  if(isset($selectNumber[1])){
-                    // $prefixNumber = explode('.', $selectNumber[1]);
-                    // $exactPrefix = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$prefixNumber[0]);
-                    $allPhoneData.="<tr><td>".$k++."</td><td>".$phoneName."</td><td><b>01".$selectNumber[1]."</b></td></tr>";
+                  $selectNumber = explode(' 01',$selectContactNumber[0]['text']);
+                  $selectNumber2=explode('01',$selectContactNumber[0]['text']);
+                  $selectNumber = isset($selectNumber[1]) ? $selectNumber : $selectNumber2;
+                  if(isset($selectNumber[1]) && isset($selectPhoneName[0]['children'][0]['text'])){
+                    $phoneName=$selectPhoneName[0]['children'][0]['text'];
+                    $prefixNumber = preg_replace('/[^A-Za-z0-9]/', '', $selectNumber[1]);
+                    $exactPrefix = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$prefixNumber);
+                    if(strlen($exactPrefix[0])==8)
+                      $allPhoneData.="<tr><td>".$k++."</td><td>".$phoneName."</td><td><b>01".$exactPrefix[0]."</b></td></tr>";
                   }
               }
           }
